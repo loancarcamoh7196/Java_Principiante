1cd7bb1d428357f8325ce9462beadce4bd4a7b0f
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-04-12 17:43:15 -0400

    Clase de video 4

---
+++ Suma.java
@@ -0,0 +1,11 @@
+public class Suma {
+  public static void main(String args[]) {
+    int num_uno = 5;
+    int num_dos = 3;
+    int contenedor = 0;
+
+    contenedor = num_uno + num_dos;
+
+    System.out.println("El resultado de tu suma es: " + contenedor);
+  }
+}
