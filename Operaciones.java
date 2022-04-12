bb3b5cf19a74e6aa23e1e268b00ade33d964f913
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-04-12 17:52:10 -0400

    Clase de video 5

---
+++ Operaciones.java
@@ -0,0 +1,11 @@
+public class Operaciones {
+  public static void main(String[] args) {
+
+    int num_1 = 4;
+    int num_2 = 4;
+    int res = 0;
+
+    res = (num_1 + num_2) /2 ;
+    System.out.println("El resultado de la operacion es: " + res);
+  }
+}
